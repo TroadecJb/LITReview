412adb0926f04659d43baac55adb2a605840346c
TroadecJb <110687346+TroadecJb@users.noreply.github.com>
2023-05-05 21:07:29 +0200

    label update

--- LITReview/publications/models.py
+++ LITReview/publications/models.py
@@ -11,7 +11,7 @@ RATING_RANGE = range(5)
 class Ticket(models.Model):
     """Class for a standard ticket"""
 
-    title = models.CharField(max_length=128, verbose_name="title")
+    title = models.CharField(max_length=128, verbose_name="titre")
     description = models.TextField(
         max_length=2048, blank=True, verbose_name="description"
     )
@@ -40,13 +40,15 @@ class Review(models.Model):
 
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE, null=True)
     rating = models.PositiveSmallIntegerField(
-        validators=[MinValueValidator(0), MaxValueValidator(5)], null=True
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+        null=True,
+        verbose_name="note",
     )
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
     headline = models.CharField(
-        max_length=128, blank=True, verbose_name="headline"
+        max_length=128, blank=True, verbose_name="titre"
     )
-    body = models.TextField(max_length=8192, blank=True, verbose_name="body")
+    body = models.TextField(max_length=8192, blank=True, verbose_name="texte")
     time_created = models.DateTimeField(auto_now_add=True)
